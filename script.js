625e14e95d914b79a024926510336e56f60d7c38
Makanjuola Martins <Martins423love@gmail.com>
2022-02-19 16:46:49 +0100

    second commit

--- script.js
+++ script.js
@@ -10,10 +10,19 @@ const title = document.querySelector('#title');
 const cover = document.querySelector('#cover');
 
 // Song titles
-const songs = ["All_Time_Low", "Conversations_With_My_Wife", "Like_This", "Maybe_IDK", "My_Stress", "Only"]
+const songs = [
+  "All_Time_Low", 
+  "Conversations_With_My_Wife", 
+  "Like_This", 
+  "Maybe_IDK", 
+  "My_Stress", 
+  "Only",
+  "Omo_ope",
+  "Calm_down",
+]
 
 // Keep track of song
-let songIndex = 5;
+let songIndex = 7;
 
 // Initially load song details into DOM
 loadSong(songs[songIndex]);
@@ -69,6 +78,20 @@ function nextSong() {
     playSong();
 }
 
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
 // Event listeners
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
@@ -83,3 +106,7 @@ playBtn.addEventListener('click', () => {
 // Change song
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+
+audio.addEventListener('timeupdate', updateProgress);
+
+progressContainer.addEventListener('click', setProgress)

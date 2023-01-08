69c70c246a27f4e62108b845cf06332bd11fbf1c
Makanjuola Martins <Martins423love@gmail.com>
2023-01-08 03:00:06 +0100

    updated the dark/light mode and clock

--- script.js
+++ script.js
@@ -9,6 +9,52 @@ const progressContainer = document.querySelector('.progress_container');
 const title = document.querySelector('#title');
 const cover = document.querySelector('#cover');
 
+const hourEL = document.querySelector('.hour')
+const minuteEL = document.querySelector('.minute')
+const secondEL = document.querySelector('.seconds')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggle = document.querySelector('.toggle')
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+// dark/light mode
+toggle.addEventListener('click', (e) => {
+  const html = document.querySelector('html')
+  if (html.classList.contains('dark')) {
+      html.classList.remove('dark')
+      e.target.innerHTML = 'Dark'
+  } else {
+      html.classList.add('dark')
+      e.target.innerHTML = 'Light'
+  }
+})
+
+// time fuc
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hours = time.getHours();
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? 'PM' : 'AM'
+
+  hourEL.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+  minuteEL.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+  secondEL.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+  timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 // Song titles
 const songs = [
   "All_Time_Low", 
@@ -108,6 +154,11 @@ function addDarkmodeWidget() {
   new Darkmode().showWidget();
 }
 window.addEventListener('load', addDarkmodeWidget);
+
+// ctrl time
+setTime();
+
+setInterval(setTime, 1000)
 
 // Change song
 prevBtn.addEventListener('click', prevSong);
